027aab1759898b7ec443cfdf21abd3a051dc8628
Manjeet Kumar <themanjeetkr@gmail.com>
2026-04-11 23:21:29 +0530

    add extras

--- array.js
+++ array.js
@@ -1,3 +1,6 @@
 var array = [1, 2, 3, 4, 5];
 array.push(6);
 console.log(array);
+var stringarray = ["themanjeet", "kr", "hello"];
+stringarray.push("world");
+console.log(stringarray);
--- array.ts
+++ array.ts
@@ -1,3 +1,7 @@
 const array:number[] = [1, 2, 3, 4, 5];
 array.push(6);
 console.log(array); 
+
+const stringarray:string[]= ["themanjeet","kr","hello"];
+stringarray.push("world");
+console.log(stringarray);
