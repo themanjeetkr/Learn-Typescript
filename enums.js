9b6bac1753a9e5c76edacbf947a2e30707acbf4d
Manjeet Kumar <themanjeetkr@gmail.com>
2026-04-11 23:44:48 +0530

    added the enums

---
+++ enums.js
@@ -0,0 +1,10 @@
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections["North"] = "North";
+    CardinalDirections["East"] = "East";
+    CardinalDirections["South"] = "South";
+    CardinalDirections["West"] = "West";
+})(CardinalDirections || (CardinalDirections = {}));
+;
+console.log(CardinalDirections.North);
+console.log(CardinalDirections.West);
---
+++ enums.ts
@@ -0,0 +1,8 @@
+enum CardinalDirections {
+  North = 'North',
+  East = "East",
+  South = "South",
+  West = "West"
+};
+console.log(CardinalDirections.North);
+console.log(CardinalDirections.West);
